4f847662fdb7d72bc420b27b86dbadf76d3cc1a7
Eduardo Vazzoler <eduardotoril@gmail.com>
2024-08-27 10:56:25 -0300

    feat close cartab with click outside of it

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,8 +1,18 @@
 const cartTabToggler = document.querySelector('.cartIcon')
-const iconCartSpan = document.querySelector('.cartIcon span')
 
 cartTabToggler.addEventListener('click', (event) => {
 	event.preventDefault()
 	const cartTab = document.querySelector('.cartTabContainer')
 	cartTab.style.display = cartTab.style.display === 'none' ? 'block' : 'none'
+})
+
+document.addEventListener('click', (event) => {
+	const cartSidePanel = document.querySelector('.cartTabContainer')
+
+	if (
+		!cartTabToggler.contains(event.target) &&
+		!cartSidePanel.contains(event.target)
+	) {
+		cartSidePanel.style.display = 'none'
+	}
 })
